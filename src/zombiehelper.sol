22da26e11518f2b536acedfc825262a9f2074039
PixelFault <luca.georges-francois@epitech.eu>
2020-08-30 19:49:09 +0200

    [+]Add L3CHAP9

--- src/zombiehelper.sol
+++ src/zombiehelper.sol
@@ -7,4 +7,14 @@ contract zombiehelper {
         require(zombies[_zombieId].level >= _level);
         _;
     }
+
+    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
 }
